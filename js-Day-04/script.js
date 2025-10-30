307bf19a4ee7fda1a0304b0e8e4803bd75c9adaa
gilman qazi <gilmanqazi@gmail.com>
2025-10-30 18:13:53 +0530

    29oc assignment

--- js-Day-04/script.js
+++ js-Day-04/script.js
@@ -1,22 +1,151 @@
-// let Fn = prompt("enter your first number");
-// let op = prompt("+, -, *, /,")
-// let Sn = prompt("enter your second number")
-// let result;
+// let a = 10; 
+// let b = 3;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
 
-// Fn = Number(Fn);
-// Sn = Number(Sn);
+// let x = 5;
+// x +=3;
+// console.log(x)
 
-// if(op === "+"){
-// result = Fn+Sn;
+
+// let y = 5;
+// y -=3;
+// console.log(y)
+
+
+// let z = 5;
+// z *=3;
+// console.log(z)
+
+
+// let a = 5;
+// a /=3;
+// console.log(a)
+
+// let count = 5;
+// count++
+// console.log(count)
+
+
+// let count = 5;
+// count--
+// console.log(count)
+
+// if(10>5 && 10<20){
+//   console.log('true')
+// } else{                   //true
+//   console.log("false")
 // }
-// else if(op === "-"){
-//   result = Fn-Sn;
+// if(10>50 || 100<20){
+//   console.log('true')
+// } else{                     //false
+//   console.log("false")
 // }
-// else if(op === "*"){
-//   result = Fn*Sn;
+
+// if(!(10>5)){
+//   console.log('true')
+// } else{                       //false
+//   console.log("false")
 // }
-// else if(op === "/"){
-//   result = Fn/Sn;
+
+
+// console.log(a);
+// var a = 10;
+
+
+// console.log(b);
+// let b = 10;
+
+// test()
+// function test(){
+//   console.log("hello")
 // }
-// alert(`Result ${result}` )
+
+// let age  = Number(prompt ("Enter Your Age"));
+// if(age>18){
+//   console.log("Adult")
+// } else {
+//   console.log("Minor")
+// }
+
+// function marks(val){
+//   if(val >=90){
+//      return "A Grade"
+//   }
+//   else if(val >=75){
+//      return "B Grade"
+//   }
+//   else if(val >=50) {
+//     return "C Grade"
+//   }
+//   else return "Fail"
+// }
+// console.log(marks(100))
+
+// let marks = 10;
+//   if(marks>=90){
+//   console.log( "A Grade")
+//   }
+//   else if(marks >=75){
+//      console.log("B Grade")
+//   }
+//   else if(marks >=50) {
+// console.log("C Grade")
+//   }
+//   else {
+// console.log("Fail")
+//   } 
+
+//   console.log(marks)
+
+
+// let city = "jalna";
+
+// if(city === "jalna") {
+//   console.log('Maharasharta')
+// } else if (city === "delhi" ){
+//   console.log("capital")
+// } else{
+//   console.log("Unknown City")
+// }
+
+// let score = 40;
+// if (score >35 ? console.log('pass') : console.log('Fail'));
+
+// let temp = 40;
+// if(temp>=40? console.log("Hot"): console.log("Pleasant"));
+
+// switch (6) {
+//   case 1:
+//     console.log("Monday")
+//     break;
+//   case 2:
+//     console.log("Tuesday")
+//     break;
+//   case 3:
+//     console.log("Wednesday")
+//     break;
+//   case 4:
+//     console.log("Friday")
+//     break;
+//   case 5:
+//     console.log("Saturday")
+//     break;
+//   case 6:
+//     console.log("Sunday")
+//     break;
+//     default : console.log("Invalid Day")
+// }
+
+// let age = 19;
+// let country = "India"
+// if(age>18 && country == "India"){
+//   console.log("Eligible For Vote")
+// } else {
+//   console.log("Not Eligible")
+// }
+
 
