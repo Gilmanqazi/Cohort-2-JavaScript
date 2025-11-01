9e2937c74e89ed250918283071852f4ee961c24b
gilman qazi <gilmanqazi@gmail.com>
2025-11-01 11:31:51 +0530

    js-Day-06-Controll-flow

---
+++ js-Day-06/script.js
@@ -0,0 +1,29 @@
+// for(let i = 1; i<11;i++){
+//   console.log(i)
+// }
+
+// for (let i = 1; i<20;i++){
+//   if(i%2===0){
+//     console.log(i)
+//   }
+// }
+
+
+// let sum = 0;
+// for(let i = 1; i<=100;i++){
+//   sum= sum + i;
+// }
+// console.log("sum of 1 to 100 is ", sum)
+
+// let num = prompt("enter a number ");
+// for(let i = 1;i<10; i++){
+//     console.log(num * i)};
+//   let count = 0;
+// for (let i=1;i<50;i++){
+//   if(i%5===0){
+//     count++
+//     console.log(i)
+//   }
+// }
+
+// console.log("Number is Divide by 5 is (Matlab Ki 5 Number 9 Baar Divide Hua):",count)
