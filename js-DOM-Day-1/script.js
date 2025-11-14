bc7a41b5ac58a1d5777d89bd1fc2c3122e3ca2ad
gilman qazi <gilmanqazi@gmail.com>
2025-11-14 08:50:00 +0530

    Added Reset Btn

--- js-DOM-Day-1/script.js
+++ js-DOM-Day-1/script.js
@@ -1,5 +1,6 @@
 let inc = document.querySelector("#inc");
 let dec = document.querySelector("#dec");
+let reset = document.querySelector("#reset")
 let h1 = document.querySelector("h1");
 
 let a = 0;
@@ -10,5 +11,10 @@ inc.addEventListener("click",function(){
 
 dec.addEventListener("click",function(){
   a--
+  h1.innerHTML = a
+})
+
+reset.addEventListener("click",function(){
+  a = 0
   h1.innerHTML = a
 })
