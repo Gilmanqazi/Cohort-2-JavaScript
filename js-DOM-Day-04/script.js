f4b253d29ed86793c712d654f4ad962c0e6e940f
gilman qazi <gilmanqazi@gmail.com>
2025-11-19 15:36:21 +0530

    added random downloading time

--- js-DOM-Day-04/script.js
+++ js-DOM-Day-04/script.js
@@ -3,9 +3,13 @@ let btn = document.querySelector("button");
 let reset = document.querySelector("#btn");
 let h2 = document.querySelector("h2")
 let ine = document.querySelector(".inner")
+let h4 = document.querySelector(".h4")
 
 btn.addEventListener("click",function(){
   grow = 0
+
+  let num = 50 + Math.floor(Math.random()*50);
+  console.log(num)
  let clear = setInterval(() => {
   grow++
   h2.innerHTML = grow +"%"
@@ -13,14 +17,15 @@ btn.addEventListener("click",function(){
   btn.style.opacity = "0.7"
   ine.style.width = grow + "%"
   btn.textContent = "Downloading..."
- }, 50);
+ }, num);
 
 
  setTimeout(() => {
   clearInterval(clear)
   btn.style.opacity = "0.9"
   btn.textContent = "Downloaded"
- },5000);
+  h4.textContent = `Downloaded in ${num/10} seconds`
+ },num*100);
  
 })
 reset.addEventListener("click", function () {
@@ -31,6 +36,7 @@ reset.addEventListener("click", function () {
   ine.style.width = "";
   reset.style.pointerEvents = "none";
   reset.style.opacity = "0.4";
+  h4.textContent = ""
 
   setTimeout(() => {
     reset.style.opacity = "1";
